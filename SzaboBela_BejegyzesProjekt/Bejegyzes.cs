82783d0f528461ae7adff62f8fe18beb85ea8ae4
Béci Szabó <szabo.beci10@gmail.com>
2023-09-26 08:16:29 +0200

    Bejegyzes class letrehzosáa

---
+++ SzaboBela_BejegyzesProjekt/Bejegyzes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzaboBela_BejegyzesProjekt
+{
+    internal class Bejegyzes
+    {
+        private string szerzo;
+        private string tartalom;
+        private int likeok;
+        private DateTime letrejott;
+        private DateTime szerkesztve;
+
+        public string Szerzo { get => szerzo;}
+        public string Tartalom { get => tartalom; set => tartalom = value; }
+        public int Likeok { get => likeok; set => likeok = value; }
+        public DateTime Letrejott { get => letrejott; set => letrejott = value; }
+        public DateTime Szerkesztve { get => szerkesztve; set => szerkesztve = value; }
+    }
+}
