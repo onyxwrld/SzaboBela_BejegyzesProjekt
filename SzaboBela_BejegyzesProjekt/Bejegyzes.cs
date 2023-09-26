c0f5902e32b88742ff80a910940f5a6fe6d19294
Béci Szabó <szabo.beci10@gmail.com>
2023-09-26 08:38:18 +0200

    2b - elkészítése

--- SzaboBela_BejegyzesProjekt/Bejegyzes.cs
+++ SzaboBela_BejegyzesProjekt/Bejegyzes.cs
@@ -16,8 +16,32 @@ namespace SzaboBela_BejegyzesProjekt
 
         public string Szerzo { get => szerzo;}
         public string Tartalom { get => tartalom; set => tartalom = value; }
-        public int Likeok { get => likeok; set => likeok = value; }
-        public DateTime Letrejott { get => letrejott; set => letrejott = value; }
-        public DateTime Szerkesztve { get => szerkesztve; set => szerkesztve = value; }
+        public int Likeok { get => likeok;}
+        public DateTime Letrejott { get => letrejott;}
+        public DateTime Szerkesztve { get => szerkesztve;}
+
+        public Bejegyzes(string szerzo, string tartalom)
+        {
+
+        }
+
+        public Bejegyzes(string szerzo, string tartalom, int likeok, DateTime letrejott, DateTime szerkesztve)
+        {
+            this.szerzo = szerzo;
+            this.tartalom = tartalom;
+            this.likeok = 0;
+            this.letrejott = DateTime.Now;
+            this.szerkesztve = szerkesztve;
+        }
+
+        public void Like()
+        {
+            this.likeok++;
+        }
+        public override string ToString()
+        {
+            string szerkesztveSzoveg = this.Szerkesztve == this.Letrejott ? "" : $"Szerkeszve: {this.Szerkesztve}";
+            return $"{this.Szerzo} - {this.Likeok} - {this.Letrejott} {szerkesztveSzoveg}: {this.Tartalom}";
+        }
     }
 }
--- SzaboBela_BejegyzesProjekt/Program.cs
+++ SzaboBela_BejegyzesProjekt/Program.cs
@@ -8,8 +8,30 @@ namespace SzaboBela_BejegyzesProjekt
 {
     internal class Program
     {
+
+        static List<Bejegyzes> lista = new List<Bejegyzes>();
+        static void feladat2b()
+        {
+            //Ezt a feladatot így értelmeztem, nem vagyok biztos benne hogy ezt kérte tőlem a szerző.
+            Console.WriteLine("Adjon meg darabszámot hogy hány bejegyzést szeretne hozzáadni:");
+            int szam = int.Parse(Console.ReadLine());
+            for (int i = 0; i < szam; i++)
+            {
+                Console.WriteLine("Adjon meg egy szerzőt és tartalmat");
+                string szerzo = Console.ReadLine();
+                string tartalom = Console.ReadLine();
+                Bejegyzes a3 = new Bejegyzes(szerzo, tartalom);
+            }
+        }
+
         static void Main(string[] args)
         {
+            Bejegyzes a1 = new Bejegyzes("geza","rajosan geza");
+            Bejegyzes a2 = new Bejegyzes("agoston", "nyomjad geza");
+            lista.Add(a1);
+            lista.Add(a2);
+            feladat2b();
+            Console.ReadKey();
         }
     }
 }
